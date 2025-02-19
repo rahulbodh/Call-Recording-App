adca060fd9ee0b211c51e3996d724a44a305486e
Rahul <rahulbodh819@gmail.com>
2025-02-19 15:01:26 +0530

    ask permission for record audio

--- app/src/main/java/com/example/callrecorder/MainActivity.kt
+++ app/src/main/java/com/example/callrecorder/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.callrecorder
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.callrecorder.databinding.ActivityMainBinding
@@ -10,6 +17,7 @@ import com.example.callrecorder.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,5 +28,42 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        checkAndRequestAudioPermission();
+    }
+
+    private fun checkAndRequestAudioPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // Permission is granted, proceed with audio recording
+                Toast.makeText(this, "Permission already granted!", Toast.LENGTH_SHORT).show()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO) -> {
+                // Show rationale and request permission
+                requestAudioPermission()
+            }
+            else -> {
+                // Directly request for permission
+                requestAudioPermission()
+            }
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission granted
+                Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show()
+            } else {
+                // Permission denied
+                Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private fun requestAudioPermission() {
+        requestPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
     }
 }
